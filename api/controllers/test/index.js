473d6e0d0d761a4818d1c15267b9193c2bf89add
YushchenkoAndrew <Andrew10122000@gmail.com>
2020-09-21 01:06:25 +0300

    Receive information from github api

--- api/controllers/test/index.js
+++ api/controllers/test/index.js
@@ -26,11 +26,55 @@ module.exports = {
   fn: async function ({ id }) {
     // All done.
     sails.log("Action testing...");
-    sails.log(`id = ${id}`);
+    sails.log();
+    // sails.log(`id = ${id}`);
 
-    if (!id) {
-      throw "notFound";
-    }
+    // if (!id) {
+    // throw "notFound";
+    // }
+
+    var repos = {};
+
+    let request = require("request");
+
+    let option = {
+      url: "https://api.github.com/users/YushchenkoAndrew/repos",
+      headers: {
+        "User-Agent": "YushchenkoAndrew",
+      },
+    };
+
+    request.get(option).on("response", (res) => {
+      var data = [];
+
+      sails.log(`URL: ${res.url}\n`);
+      sails.log(`Status: ${res.statusCode}\n`);
+      // if (res.statusCode !== 200) throw "notFound";
+
+      res.setEncoding("utf-8");
+      res.on("data", (chunk) => data.push(chunk));
+
+      res.on("end", () => {
+        let obj = JSON.parse(data.join(""));
+
+        console.log("Repositories:");
+        for (let i in obj) {
+          console.log(`    ${obj[i].name}`);
+          repos[i] = {};
+
+          option.url = `https://api.github.com/repos/YushchenkoAndrew/${obj[i].name}/languages`;
+
+          request.get(option).on("response", (res) => {
+            res.setEncoding("utf-8");
+            res.on("data", (chunk) => {
+              // repos[res.url.split("/").slice(-2)[0]] = { ...JSON.parse(chunk) };
+              console.log(chunk);
+            });
+          });
+        }
+      });
+    });
+    // console.dir(result, { depth: null });
 
     // return "Test";
   },
